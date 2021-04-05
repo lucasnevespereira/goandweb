676de4cc7d56dae3adcfa659e68316e55db1ee2f
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-05 18:05:05 +0200

    added web-http

--- web-http/main.go
+++ web-http/main.go
@@ -1,1 +1,23 @@
-package web_http
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// Log request info
+	fmt.Printf("%v %v \n", r.Method, r.URL)
+	// Write a message as an HTTP response
+	fmt.Fprintf(w, "Hello There!")
+}
+
+func main() {
+
+	http.HandleFunc("/", hello)
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
