f58f1c7cb52ba9c099550317835d263fcff74633
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-05 20:48:13 +0200

    decode json

--- web-json/main.go
+++ web-json/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -15,35 +16,73 @@ type Address struct {
 
 type User struct {
 	Name string `json:"name"`
-	Password string `json:"-"`
+	Password string `json:"password"`
 	Email string `json:"email"`
 	Address Address `json:"address"`
 }
 
-func users(w http.ResponseWriter, r *http.Request) {
-	users := []User{
-		{
-			Name: "Bob",
-			Password: "secret",
-			Email: "bob@golang.org",
-			Address: Address{
-				Street: "15 rue Hade",
-				City: "Paris",
-				Country: "France",
-			},
+var users = []User{
+	{
+		Name: "Bob",
+		Password: "secret",
+		Email: "bob@golang.org",
+		Address: Address{
+			Street: "15 rue Hade",
+			City: "Paris",
+			Country: "France",
 		},
-		{
-			Name: "Alice",
-			Password: "test-secret",
-			Email: "alice@golang.org",
-			Address: Address{
-				Street: "42 rue Elle",
-				City: "Paris",
-				Country: "",
-			},
+	},
+	{
+		Name: "Alice",
+		Password: "test-secret",
+		Email: "alice@golang.org",
+		Address: Address{
+			Street: "42 rue Elle",
+			City: "Paris",
+			Country: "",
 		},
+	},
+}
+
+type PasswordJsonBody struct {
+	UserIndex int `json:"user_index"`
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+	NewPasswordRepeat string `json:"new_password_repeat"`
+}
+
+func updatePassword(w http.ResponseWriter, r *http.Request) {
+	var p PasswordJsonBody
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
+	log.Printf("Parsed struct %v", p)
+
+	if p.UserIndex < 0 || p.UserIndex > len(users) -1 {
+		msg := fmt.Sprintf("Invalid index. got user_index=%v. valid range=[0,%v]", p.UserIndex, len(users) - 1)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	u := users[p.UserIndex]
+	if u.Password != p.OldPassword {
+		http.Error(w, "Old Password do not match", http.StatusBadRequest)
+		return
+	}
+
+	if p.NewPassword != p.NewPasswordRepeat {
+		http.Error(w, "New Password do not match", http.StatusBadRequest)
+		return
+	}
+
+	u.Password = p.NewPassword
+	fmt.Fprintf(w, "Password updated ")
+}
+
+func usersList(w http.ResponseWriter, r *http.Request) {
 	// Encode Users in JSON
 	b, err := json.MarshalIndent(users, "", "  ")
 	if err != nil {
@@ -56,7 +95,8 @@ func users(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/users", users)
+	http.HandleFunc("/users", usersList)
+	http.HandleFunc("/update_password", updatePassword)
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
