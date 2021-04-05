97f06d58d5ae0dff0021fc9fa408bc15a7559e1c
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-05 18:37:34 +0200

    Handle POST form value

--- web-http/main.go
+++ web-http/main.go
@@ -21,11 +21,36 @@ func search(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Searching for term '%v' in page %v. \n", t, p)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "login.html")
+		return
+
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "Parsed Form method failed. err=%v \n", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Go login POST. value=%v\n", r.PostForm)
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if username == "Go" && password == "test" {
+			fmt.Fprintf(w, "You are logged in \n")
+		} else {
+			fmt.Fprintf(w, "Wring username / password \n")
+		}
+	}
+}
+
 func main() {
 
 	// Assign a route to a func
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/search", search)
+	http.HandleFunc("/login", login)
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
